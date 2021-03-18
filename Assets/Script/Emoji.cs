32f2c079d748e968e7869e1d76ee50dbbbd7fb3b
Haochii <68538182+Haochii@users.noreply.github.com>
2021-03-18 12:32:01 +0800

    V2.0
    Migrating to ShadowSDK 4.1.4

---
+++ Assets/Script/Emoji.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Emoji : MonoBehaviour
+{
+    public int emojiType = 0;
+}
---
+++ Assets/Script/EmojiTemplate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SC.XR.Unity.Module_InputSystem;
+
+public class EmojiTemplate : MonoBehaviour
+{
+    public bool isEditing = false;
+    public int emojiType = 0;
+    public Vector3 originPos;
+    public Quaternion originRot;
+    public Vector3 originScale;
+    private bool isRecorded = false;
+    public SCPointEventData resetEvent;
+    public void reset()
+    {
+        transform.position = originPos;
+        transform.rotation = originRot;
+        transform.localScale = originScale;
+
+        isEditing = false;
+        
+        Debug.Log("Template Reset");
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        originPos = transform.position;
+        originRot = transform.rotation;
+        originScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void editing()
+    {
+        isEditing = true;
+        Debug.Log("Editing Template");
+    }
+}
