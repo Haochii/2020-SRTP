6463b4bf6a12504d1612b5f839115680b6a84a7d
Haochii <68538182+Haochii@users.noreply.github.com>
2021-03-19 00:33:04 +0800

    Added Visual Effect
    Visual Effect of explosion

---
+++ Assets/Script/CubeCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CubeCollision : MonoBehaviour
+{
+    float ttl = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void des()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "sculpture")
+        {
+            ParticleSystem exp = GetComponent<ParticleSystem>();
+            exp.Play();
+            this.Invoke("des", ttl);            
+            Debug.Log("OK");
+        }
+    }
+}
